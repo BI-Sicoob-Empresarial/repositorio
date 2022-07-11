05c5fc12a1f651a677509e0b3f3f727664854c42
BI-Sicoob-Empresarial <106930821+BI-Sicoob-Empresarial@users.noreply.github.com>
2022-07-11 16:32:02 -0300

    Update DADOS_PLANO_EXPANSAO.R

--- PainelPlanoExpansao/SCRIPT/DADOS_PLANO_EXPANSAO.R
+++ PainelPlanoExpansao/SCRIPT/DADOS_PLANO_EXPANSAO.R
@@ -1,3 +1,7 @@
 url<-'https://github.com/BI-Sicoob-Empresarial/repositorio/raw/main/Base_Dados/Plano_de_Expansao/agencias_rio_verde.xlsx'
 GET(url, write_disk(TF <- tempfile(fileext = ".xlsx")))
 agencias_rio_verde = read_excel(TF)
+
+url<-'https://github.com/BI-Sicoob-Empresarial/repositorio/raw/main/Base_Dados/Plano_de_Expansao/ConsolidadoRV%20-%20Tratado.xlsx'
+GET(url, write_disk(TF <- tempfile(fileext = ".xlsx")))
+leads = read_excel(TF)
